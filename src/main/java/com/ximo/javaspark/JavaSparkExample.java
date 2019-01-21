06f527136dd45dc30f69fd6a977db58252e96f63
朱文赵 <zwz@servyou.com.cn>
2019-01-21 14:35:11 +0800

    java and scala 的单词计数版本

--- src/main/java/com/ximo/javaspark/JavaSparkExample.java
+++ src/main/java/com/ximo/javaspark/JavaSparkExample.java
@@ -1,7 +1,17 @@
 package com.ximo.javaspark;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @author 朱文赵
@@ -11,8 +21,28 @@ public class JavaSparkExample {
 
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("scala-spark-in-action");
-        SparkContext sparkContext = new SparkContext(sparkConf);
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+        JavaRDD<String> input = sparkContext.textFile("README.md");
+        input.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterator<String> call(String s) throws Exception {
+                return Arrays.asList(s.split(" ")).iterator();
+            }
+        });
 
+        JavaRDD<String> words = input.flatMap(line -> Arrays.stream(line.split(" ")).iterator());
+
+        // pdf中自带的一种写法 统计词频
+        JavaPairRDD<String, Integer> counts = words
+                .mapToPair(s -> new Tuple2<>(s, 1))
+                .reduceByKey((x, y) -> x + y);
+
+        // 按照自己的理解编写 统计词频
+        long myWordCount = words.groupBy((Function<String, String>) v1 -> v1).count();
+        // 词频统计 转化为map
+        Map<String, Integer> wordCountMap = counts.collectAsMap();
+        // 保存为文件
+        counts.saveAsTextFile("anywhere");
     }
 
 
--- src/main/scala/com/ximo/scalaspark/ScalaSparkExample.scala
+++ src/main/scala/com/ximo/scalaspark/ScalaSparkExample.scala
@@ -7,16 +7,23 @@ import org.apache.spark.{SparkConf, SparkContext}
   *
   * @author 朱文赵
   */
-class ScalaSparkExample {
+object ScalaSparkExample {
 
   def main(args: Array[String]): Unit = {
+    wordCount()
+  }
+
+  /**
+    * 统计计数
+    *
+    */
+  def wordCount(): Unit ={
     val conf = new SparkConf().setMaster("local").setAppName("scala-spark-in-action")
     val sc = new SparkContext(conf)
-    val lines = sc.textFile("README.md")
-    lines.flatMap(line => line.split(" ")).groupBy(identity).count()
-
-
-
+    val input = sc.textFile("D:\\jupyter-notebook\\README.md")
+    val words = input.flatMap(line => line.split(" "))
+    val counts = words.map(word => (word, 1)).reduceByKey((x, y) => x + y)
+    println(counts.collectAsMap())
   }
 
 }
